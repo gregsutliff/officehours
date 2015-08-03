3cfd3bcde959bd7b329b4297943781d1eeff8154
Greg Sutliff <gregsutliff@gmail.com>
2015-08-03 05:49:37 -0500

    Generate Devise user model and associated Devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+=======
+  devise_for :users
+  root 'office_hours#open'
+>>>>>>> 156688b... Generate Devise user model and associated Devise views
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
