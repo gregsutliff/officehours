1e61942862e39c7db1873a70150287e273c35e8a
Greg Sutliff <gregsutliff@gmail.com>
2015-08-02 20:50:56 -0500

    Add default cuke steps to authentication_steps.rb

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,23 @@
+Given(/^I am logged in$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am an organizer$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I log in as an administrator$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I visit the login page$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am a department curator$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am an organizer$/) do
+    pending # express the regexp above with the code you wish you had
+end
