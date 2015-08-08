bff103fa7213441ab2ec46a6ccca8e6d9c4a2e31
Greg Sutliff <gregsutliff@gmail.com>
2015-08-08 13:10:55 -0500

    Delete all feature tests
    Cucumber is a bit overwhelming at the moment. I
    should not have written all of the feature tests
    at once. Perhaps try again later

--- features/step_definitions/curator_steps.rb
+++
@@ -1,7 +0,0 @@
-Then(/^I will be redirected to my department listing$/) do
-    pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I am redirected to the department curation page$/) do
-    pending # express the regexp above with the code you wish you had
-end
