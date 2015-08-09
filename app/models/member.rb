c751641a6282d38faab39683b276917efd946faf
Greg Sutliff <gregsutliff@gmail.com>
2015-08-09 05:14:44 -0500

    wip

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,5 @@
 class Member < ActiveRecord::Base
   validates :firstname, :lastname, :uin, :presence => true
   validates :uin, :uniqueness => true
+  has_many :office_hours, dependent: :destroy
 end
