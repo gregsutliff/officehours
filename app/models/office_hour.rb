a4f57d12eb21482fdfc700495c5e354e14149342
Greg Sutliff <gregsutliff@gmail.com>
2015-08-09 05:11:22 -0500

    Remove swap files

--- app/models/office_hour.rb
+++ app/models/office_hour.rb
@@ -1,7 +1,7 @@
 class OfficeHour < ActiveRecord::Base
   validates :day, :start, :stop, :member_id,  presence: true
 
-  belongs_to :member, dependent: :destroy
+  belongs_to :member
 
   scope :open, -> (day, time) { where(:day => day).where("? BETWEEN TIME(start) AND TIME(stop)", time)}
 
