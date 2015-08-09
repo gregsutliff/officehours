a2600ac4658476113f0125fc2c696fb18ee1eefe
Greg Sutliff <gregsutliff@gmail.com>
2015-08-09 07:13:29 -0500

    Refactored OfficeHours controller index action
    Moved curator redirect logic into a private method

--- app/controllers/office_hours_controller.rb
+++ app/controllers/office_hours_controller.rb
@@ -1,11 +1,20 @@
 class OfficeHoursController < ApplicationController
  
   def index
+    redirect_curators
+    @open_hours = OfficeHour.open(1,'11:00')
+  end
+
+
+
+
+  private
+
+  def redirect_curators
     if current_user.role == 2
       flash[:notice] = "Sorry, you're not authorized to access that page"
       redirect_to(curate_url)
     end
-    @open_hours = OfficeHour.open
   end
 
 end
