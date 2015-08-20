dbd9cbdf343f205bd459a11056a52a8f74c9f29a
Greg Sutliff <gregsutliff@gmail.com>
2015-08-20 09:07:09 -0500

    Create Department model
    Each member belongs to one department. This may present
    problems in the future, as some members may actually
    be enrolled in/working in multiple departments.

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,7 @@ class Member < ActiveRecord::Base
   validates :firstname, :lastname, :uin, :presence => true
   validates :uin, :uniqueness => true
   belongs_to :office
+	belongs_to :department
 	has_one :building, :through => :office
   has_many :office_hours, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819145819) do
+ActiveRecord::Schema.define(version: 20150820134858) do
 
   create_table "buildings", force: :cascade do |t|
     t.string   "fullname"
