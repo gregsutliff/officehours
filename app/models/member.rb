8e3eb6ce88b12bb05e69fc5f40b506f240caf2d3
Greg Sutliff <gregsutliff@gmail.com>
2015-08-27 06:08:26 -0500

    Add Emails to Curator Table
    Since some emails will be nil, changed the delegations
    accordingly, along with the _member_table.html partial

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,11 +3,15 @@ class Member < ActiveRecord::Base
   validates :uin, :uniqueness => true
   belongs_to :office
 	belongs_to :department
+	has_one :email
 	has_one :building, :through => :office
   has_many :office_hours, dependent: :destroy
+
 	delegate :name, :to => :department, :prefix => true
 	delegate :fullname, :to => :building, :prefix => true
 	delegate :name, :to => :office, :prefix => true
+	delegate :uic_email, :to => :email, :allow_nil => true
+	delegate :non_uic_email, :to => :email, :allow_nil => true
 
 	def fullname
 		firstname + " " + lastname
