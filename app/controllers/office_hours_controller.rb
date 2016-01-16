a1c43a31db10952fcf95d0cafaca67f4f87e52ef
Greg Sutliff <gregsutliff@gmail.com>
2016-01-15 21:05:55 -0600

    Elastic Search is WORKINGgit add .git add .

--- app/controllers/office_hours_controller.rb
+++ app/controllers/office_hours_controller.rb
@@ -1,9 +1,20 @@
 class OfficeHoursController < ApplicationController
  
   def index
+
     redirect_curators
+    hour = Time.now.hour
+    minute = Time.now.min 
+    second = Time.now.sec
+    time = Time.new(2000, 01, 01, hour, minute, second)
+    time = time.strftime('%Y-%m-%dT%H:%M:%S')
     if params[:query].present?
-      @open_hours = OfficeHour.search(params[:query])
+      @open_hours = OfficeHour.search params[:query], where: {
+        start: {lt: time},
+        stop: {gt: time},
+        day: day,
+        },
+        order: {stop: :desc}
     else 
       @open_hours = OfficeHour.includes(:member, :building, :office, :department).open(day, now).order('stop desc')
     end
