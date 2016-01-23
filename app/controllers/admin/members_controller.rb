8e6946b3210592e4ab6099721d96ba32e732a51a
Greg Sutliff <gregsutliff@gmail.com>
2016-01-23 15:09:40 -0600

    Fix n+1 queries on admin members table

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -3,7 +3,7 @@ class Admin::MembersController < ApplicationController
   #index provides an alphabetized list of all members
   def index
     redirect_non_admin
-    @members = Member.all
+    @members = Member.includes(:department, :building, :office).order('departments.name').order('members.lastname')
   end
 end
 
