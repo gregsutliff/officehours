4b9a983c50aae17f213b1f837f78616312f7f077
Greg Sutliff <gregsutliff@gmail.com>
2016-01-23 18:53:07 -0600

    Create admin ability to update, delete, and edit departments

--- app/controllers/admin/departments_controller.rb
+++ app/controllers/admin/departments_controller.rb
@@ -1,19 +1,36 @@
 class Admin::DepartmentsController < ApplicationController
   def index
+    @departments = Department.all
   end
 
   def new
+    @department = Department.new
   end
 
   def create
+    @department = Department.create(admin_params)
+    redirect_to admin_departments_path
   end
 
   def edit
+    @department = Department.find(params[:id])
   end
 
   def update
+    @department = Department.find(params[:id])
+    @department.update(admin_params)
+    redirect_to admin_departments_path
   end
 
   def destroy
+    @department = Department.find(params[:id])
+    @department.delete
+    redirect_to admin_departments_path
   end
+end
+
+private
+
+def admin_params
+  params.require(:department).permit(:name, :abbrev)
 end
