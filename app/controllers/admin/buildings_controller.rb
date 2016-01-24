3bdc2ed75bf416d8024baa58d58e8ce21b8a017f
Greg Sutliff <gregsutliff@gmail.com>
2016-01-23 18:35:59 -0600

    Add building edit and destroy capabilities

--- app/controllers/admin/buildings_controller.rb
+++ app/controllers/admin/buildings_controller.rb
@@ -7,17 +7,31 @@ class Admin::BuildingsController < ApplicationController
   end
 
   def new
+    @building = Building.new
   end
 
   def create
+    Building.create(admin_params)
   end
 
   def edit
+    @building = Building.find(params[:id])
   end
 
   def update
+    building = Building.find(params[:id])
+    building.update(admin_params)
+    redirect_to admin_buildings_path
   end
 
   def destroy
+    building = Building.find(params[:id])
+    building.delete
   end
+end
+
+private
+
+def admin_params
+  params.require(:building).permit(:fullname, :abbrev)
 end
