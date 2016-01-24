645c4d72042e7bb3ff8bd7fd5b83c809d160791f
Greg Sutliff <gregsutliff@gmail.com>
2016-01-24 08:49:11 -0600

    Add pagination to member table

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,7 +2,7 @@ class Admin::MembersController < ApplicationController
   helper_method :sort_column, :sort_direction
   def index
     redirect_non_admin
-    @members = Member.includes(:department, :building, :office_hours).order(sort_column + ' ' + sort_direction)
+    @members = Member.includes(:department, :building, :office_hours).order(sort_column + ' ' + sort_direction).paginate(per_page: 25, page: params[:page])
   end
 
   def edit
