c981bf6b38f2a8ba35f4bf5136689a8fdf073138
Greg Sutliff <gregsutliff@gmail.com>
2016-01-27 07:57:24 -0600

    Add building modal working

--- app/controllers/admin/buildings_controller.rb
+++ app/controllers/admin/buildings_controller.rb
@@ -8,20 +8,24 @@ class Admin::BuildingsController < ApplicationController
 
   def new
     @building = Building.new
+    respond_to :html, :js
   end
 
   def create
-    Building.create(admin_params)
+    @building = Building.create(admin_params)
+    respond_to :html, :js
   end
 
   def edit
     @building = Building.find(params[:id])
+    respond_to :html, :js
   end
 
   def update
     building = Building.find(params[:id])
     building.update(admin_params)
     redirect_to admin_buildings_path
+    respond_to :html, :js
   end
 
   def destroy
