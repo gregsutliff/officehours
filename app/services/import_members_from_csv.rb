6e1431388e3a60e402cbc0840b43ecb7050447ef
Greg Sutliff <gregsutliff@gmail.com>
2016-02-12 14:35:07 -0600

    Separate hash cleaning logic from csv import logic

--- app/services/import_members_from_csv.rb
+++ app/services/import_members_from_csv.rb
@@ -1,30 +1,36 @@
 require 'csv'
-
+# ImportMembersFromCsv is a service which takes a csv containing member data
+# and uses that data to create and/or update the member database.
 class ImportMembersFromCsv
-
   def initialize(file)
     @file = file
   end
 
   def import
     CSV.foreach(@file.path, headers: true) do |row|
-      memberhash = row.to_hash
-      firstname = memberhash['firstname']
-      lastname = memberhash['lastname']
-      email = memberhash['email']
-      door = memberhash[:door]
-      department = Department.find_by(name: memberhash['department']) ||
-                   Department.create(name: memberhash['department'],
-                                     abbrev: memberhash['department'][0..3])
-      department = department.id
-      clean_hash = { firstname: firstname,
-                     lastname: lastname,
-                     email: email,
-                     department_id: department,
-                     door_number: door }
-      member = Member.find_by(email: email) || Member.new
-      member.update(clean_hash)
+      memberhash = clean_hash(row.to_hash)
+      member = Member.find_by(email: memberhash['email']) || Member.new
+      member.update(memberhash)
       member.save
     end
+  end
+
+  private
+
+  # clean_hash takes a hash that may contain keys for many more fields than we
+  # are interested in putting into our database, and it prunes it so as to
+  # return a hash containing only the stuff that we will be using.
+  def clean_hash(hash)
+    # checks to see if the department already exists and creates a new one if
+    # necessary
+    department = Department.find_by(name: hash['department']).id ||
+                 Department.create(name: hash['department'],
+                                   abbrev: hash['department'][0..3]).id
+    result = { firstname: hash['firstname'],
+               lastname: hash['lastname'],
+               email: hash['email'],
+               door_number: hash['door'],
+               department_id: department }
+    result
   end
 end
