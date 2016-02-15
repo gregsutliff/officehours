14eee0e0c6f573c31716247f86053360be9f304e
Greg Sutliff <gregsutliff@gmail.com>
2016-02-15 08:04:27 -0600

    wip

---
+++ spec/factories/csv_factory.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :csv do
+  end
+end
---
+++ spec/services/import_members_from_csv_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe '.ImportMembersFromCsv' do
+  it 'adds new members to the database'
+  it 'updates members who are already in the database'
+end
